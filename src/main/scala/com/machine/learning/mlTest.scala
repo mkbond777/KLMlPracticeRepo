0f0cc6f714ccc1773a4acfe93353e01f68017d76
Manish <mkbond777@hotmail.com>
2016-07-15 17:42:57 +0530

    created a new scala file

---
+++ src/main/scala/com/machine/learning/mlTest.scala
@@ -0,0 +1,32 @@
+package com.machine.learning
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.mllib.linalg.Vectors
+
+object mlTest {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val training = sqlContext.createDataFrame(Seq(
+      (1.0, Vectors.dense(0.0, 1.1, 0.1)),
+      (0.0, Vectors.dense(2.0, 1.0, -1.0)),
+      (0.0, Vectors.dense(2.0, 1.3, 1.0)),
+      (1.0, Vectors.dense(0.0, 1.2, -0.5))
+    )).toDF("label", "features")
+
+    val lr = new LogisticRegression()
+    lr.setMaxIter(10)
+      .setRegParam(0.01)
+
+    val model1 = lr.fit(training)
+
+    println("Model 1 was fit using parameters: " + model1.parent.extractParamMap)
+
+
+
+      }
+
+}
