eb4aabe3e42111847e598979e2984b704b50a21f
Manish <mkbond777@hotmail.com>
2016-07-15 16:09:12 +0530

    created a new scala file

--- src/main/scala/mlTest.scala
+++ src/main/scala/mlTest.scala
@@ -1,6 +1,16 @@
-/**
-  * Created by mk13935 on 7/15/2016.
-  */
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.param.ParamMap
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.sql.Row
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
 object mlTest {
-adsmnbdsm
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+      }
+
 }
