d5bded3b497c6a37acc25830270a0f3a6ef84bc1
Manish <mkbond777@hotmail.com>
2016-07-18 15:15:42 +0530

    Pipeline Example Code written

---
+++ src/test/scala/com/machine/test/PipelineExample.scala
@@ -0,0 +1,68 @@
+package com.machine.test
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.feature.{HashingTF, Tokenizer}
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.sql.Row
+
+
+object PipelineExample {
+    def main(args: Array[String]) {
+      val conf = new SparkConf().setAppName("Simple Application")
+      val sc = new SparkContext(conf)
+      val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+      // Prepare training documents from a list of (id, text, label) tuples.
+      val training = sqlContext.createDataFrame(Seq(
+        (0L, "a b c d e spark", 1.0),
+        (1L, "b d", 0.0),
+        (2L, "spark f g h", 1.0),
+        (3L, "hadoop mapreduce", 0.0)
+      )).toDF("id", "text", "label")
+
+      // Configure an ML pipeline, which consists of three stages: tokenizer, hashingTF, and lr.
+      val tokenizer = new Tokenizer()
+        .setInputCol("text")
+        .setOutputCol("words")
+      val hashingTF = new HashingTF()
+        .setNumFeatures(1000)
+        .setInputCol(tokenizer.getOutputCol)
+        .setOutputCol("features")
+      val lr = new LogisticRegression()
+        .setMaxIter(10)
+        .setRegParam(0.01)
+      val pipeline = new Pipeline()
+        .setStages(Array(tokenizer, hashingTF, lr))
+
+
+      // Fit the pipeline to training documents.
+      val model = pipeline.fit(training)
+
+      // now we can optionally save the fitted pipeline to disk
+      model.write.overwrite.save("/tmp/spark-logistic-regression-model")
+
+      // we can also save this unfit pipeline to disk
+      pipeline.write.overwrite.save("/tmp/unfit-lr-model")
+
+      // and load it back in during production
+      val sameModel = PipelineModel.load("/tmp/spark-logistic-regression-model")
+
+      // Prepare test documents, which are unlabeled (id, text) tuples.
+      val test = sqlContext.createDataFrame(Seq(
+        (4L, "spark i j k"),
+        (5L, "l m n"),
+        (6L, "mapreduce spark"),
+        (7L, "apache hadoop")
+      )).toDF("id", "text")
+
+      // Make predictions on test documents.
+      sameModel.transform(test)
+        .select("id", "text", "probability", "prediction")
+        .collect()
+        .foreach { case Row(id, text, prob, prediction) =>
+          println(s"($id, $text) --> prob=$prob, prediction=$prediction")
+        }
+  }
+}
