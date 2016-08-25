ab3d52e44529832b6643ef4c7367287c8f2a2368
Manish <mkbond777@hotmail.com>
2016-08-25 12:18:34 +0530

    Starting with twitter Sentiment Analysis

---
+++ src/main/scala/com/machine/learning/TwitterSentimentAnalysis.scala
@@ -0,0 +1,15 @@
+package com.machine.learning
+import org.apache.spark.ml.feature.{CountVectorizer, RegexTokenizer, StopWordsRemover}
+import org.apache.spark.sql.functions._
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.streaming.twitter._
+
+
+/**
+  * Created by mk13935 on 7/28/2016.
+  */
+object TwitterSentimentAnalysis {
+  
+
+}
